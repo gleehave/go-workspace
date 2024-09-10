f84aaaefe5c53c02e48d9879eaf03fb92b76d27e
leegangho <gleehave@gmail.com>
2024-09-10 17:23:12 +0900

    [add]

---
+++ src/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("HI")
+	ak47, _ := getGun("ak47")
+	musket, _ := getGun("musket")
+
+	printDetails(ak47)
+	printDetails(musket)
+}
+
+func printDetails(g IGun) {
+	fmt.Printf("Gun: %s", g.getName())
+	fmt.Println()
+	fmt.Printf("Power: %d", g.getPower())
+	fmt.Println()
+}
